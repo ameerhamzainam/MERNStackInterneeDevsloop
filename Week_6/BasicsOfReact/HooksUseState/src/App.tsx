3c7430f7ae32fa71006123908b0dfcf6d27ae795
Ameer Hamza Rubani <46670204+ameerhamzainam@users.noreply.github.com>
2025-04-22 12:33:58 +0500

    Custom Hook Created-week 6 Done

--- Week_6/BasicsOfReact/HooksUseState/src/App.tsx
+++ Week_6/BasicsOfReact/HooksUseState/src/App.tsx
@@ -5,35 +5,34 @@ import burger from './assets/beef-burger.avif'
 import pizza from './assets/Pizza.webp'
 import kofta from './assets/Moroccan Beef Kofta.jpg'
 import UserData from './Components/UserData'
+import useLocalStorage from './Components/useLocalStorage'
+
 
 const App = () => {
 
 
+  const [theme, setTheme] = useLocalStorage('theme', 'light');
 
-
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
 
   return (
-    <div >
-
-      {/* I have already made Components of Card and Form and if you want to mount them in the App component you can do it like this:
-      you can use thier components. */}
-
-
-
-
-
-<UserData/>
+    <div style={{ background: theme === 'light' ? '#fff' : '#222', color: theme === 'light' ? '#000' : '#fff', padding: 20 }}>
+      <h2>Current Theme: {theme}</h2>
+      <button className= 'bg-amber-400 px-4 py-2 mb-2 rounded'onClick={toggleTheme}>Toggle Theme</button>
+      <UserData />
+      <UserData />
+      <UserData />
+      <UserData />
 
 
+    </div>
+  );
 
+}
 
-
-
-
-
-
-
-      {/* <div className='flex gap-10 justify-center'>
+{/* <div className='flex gap-10 justify-center'>
         <Card recipeName='Burger' description='Taste The Real Burger' image={burger} />
         <Card recipeName="Pizza" description='Taste From Pizza Land' image={pizza} />
         <Card recipeName="Moroccan B.Kofta" description='Desi' image={kofta} />
@@ -42,10 +41,5 @@ const App = () => {
 
 
 
-    </div>
-
-
-  )
-}
 
 export default App
---
+++ Week_6/BasicsOfReact/HooksUseState/src/Components/useLocalStorage.tsx
@@ -0,0 +1,19 @@
+// useLocalStorage.js
+import { useState, useEffect } from 'react';
+
+function useLocalStorage(key : any , initialValue : any) {
+  // Get from localStorage or use default
+  const [value, setValue] = useState(() => {
+    const stored = localStorage.getItem(key);
+    return stored !== null ? JSON.parse(stored) : initialValue;
+  });
+
+  // Update localStorage when value changes
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
