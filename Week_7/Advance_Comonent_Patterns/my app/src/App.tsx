89c891374803166e05a2b89f202a0bfb71aaa502
Ameer Hamza Rubani <46670204+ameerhamzainam@users.noreply.github.com>
2025-04-24 19:58:31 +0500

    Started week 7 work => HOC Concepts

---
+++ Week_7/Advance_Comonent_Patterns/my app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-4xl text-white flex justify-center'>Password Generator</h1>
+    </div>
+  )
+}
+
+export default App
